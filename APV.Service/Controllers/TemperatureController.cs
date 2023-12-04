584857c5f4d0a48c87e8ad236135491b646b9533
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-04 20:05:29 +0100

    basic http service

---
+++ APV.Service/Controllers/TemperatureController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace APV.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<TemperatureController> _logger;
+
+        public TemperatureController(ILogger<TemperatureController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTemperature")]
+        public IEnumerable<TemperatureRecord> Get(int id)
+        {
+            return Enumerable.Range(1, 5).Select(index => new TemperatureRecord
+            {
+                Date = DateTime.Now,
+                TemperatureC = Random.Shared.Next(-20, 55),
+                ID = id
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ APV.Service/TemperatureRecord.cs
@@ -0,0 +1,11 @@
+namespace APV.Service
+{
+    public class TemperatureRecord
+    {
+        public DateTime? Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int ID { get; set; }
+    }
+}
