67186fac1ab259641ab3f1cc8832e50959759cfa
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-14 13:11:44 +0100

    renamed for clarity

--- APV.Service/Services/MeasurementService.cs
+++ APV.Service/Services/MeasurementService.cs
@@ -5,18 +5,18 @@ namespace APV.Service.Services
     public class MeasurementService : IMeasurementService
     {
         private readonly ILogger<MeasurementService> _logger;
-        private IDataManager<Measurement> _dbManager { get; set; }
+        private IDataManager<Measurement> _dataManager { get; set; }
 
         public MeasurementService(ILogger<MeasurementService> logger, IDataManager<Measurement> dataManager)
         {
             _logger = logger;
-            _dbManager = dataManager;
+            _dataManager = dataManager;
             _logger.LogInformation($"Measurement service created");
         }
 
         public bool IsConnected()
         {
-            bool connected = _dbManager != null && _dbManager.IsConnected();
+            bool connected = _dataManager != null && _dataManager.IsConnected();
             if (!connected)
             {
                 _logger.LogWarning($"Measurement service not connected");
@@ -31,7 +31,7 @@ namespace APV.Service.Services
             {
                 try
                 {
-                    List<Measurement>? measurements = _dbManager.GetManyData();
+                    List<Measurement>? measurements = _dataManager.GetManyData();
                     _logger.LogInformation($"Found {measurements?.Count} measurements");
                     if (measurements != null && measurements.Count > 0)
                     {
@@ -67,7 +67,7 @@ namespace APV.Service.Services
                 }
                 Measurement measurement = new Measurement(sensorID, temp, time);
                 _logger.LogInformation($"Add Measurement of sensor: {sensorID}, temperature {temp}, time: {time?.ToString()}");
-                return _dbManager != null && _dbManager.AddData(measurement);
+                return _dataManager.AddData(measurement);
             }
             _logger.LogWarning("Not added");
             return false;
