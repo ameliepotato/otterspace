d9cb79fbe99f49931f19709538b116f4d0b7d36e
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-14 11:41:40 +0100

    initial website configurations

---
+++ APV.Console/Pages/Configuration.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace APV.Console.Pages
+{
+    public class ConfigurationModel : PageModel
+    {
+        private readonly ILogger<ConfigurationModel> _logger;
+
+        public ConfigurationModel(ILogger<ConfigurationModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
