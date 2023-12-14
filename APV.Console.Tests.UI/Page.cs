d76af7768d6104ba39c00c877b60237421bce8a7
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-14 17:10:24 +0100

    UI tests for console

---
+++ APV.Console.Tests.UI/Page.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APV.Console.Tests.UI
+{
+    [TestFixture]
+    public class Page
+    {
+        protected IWebDriver _webDriver;
+
+        [SetUp]
+        public void Setup()
+        {
+            string driverPath = Environment.GetEnvironmentVariable("CHROMEDRIVERPATH_UITESTS") ??
+                Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "Drivers";
+            _webDriver = new ChromeDriver(driverPath);
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            _webDriver.Close();
+        }
+
+    }
+}
