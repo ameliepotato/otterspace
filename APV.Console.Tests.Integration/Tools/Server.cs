87d69d0403094cf8af9cd7333c1e5ebd673fd122
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-22 10:12:40 +0100

    added multiple path on same url wait for mock server

--- APV.Console.Tests.Integration/Tools/Server.cs
+++ APV.Console.Tests.Integration/Tools/Server.cs
@@ -13,6 +13,48 @@ namespace APV.Console.Tests.Integration.Tools
 {
     internal class Server
     {
+        private static Dictionary<string, string> _responseData = new Dictionary<string, string>();
+        private static bool Started = false;
+       
+        public static void AddToResponseData(string path, string responsData)
+        {
+           _responseData.Add(path, responsData);
+        }
+
+        public static void StartListening(string url)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                return;
+            }
+
+            foreach (var path in _responseData)
+            {
+
+                HttpListener listener = new HttpListener();
+                listener.Prefixes.Add(url);
+                listener.Start();
+
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = listener.GetContext();
+                string? requestUrl = context.Request.Url?.AbsoluteUri;
+                HttpListenerResponse response = context.Response;
+                if (requestUrl?.Contains(path.Key) == true)
+                {
+                    // Construct a response.
+                    string responseString = path.Value;
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    // Get a response stream and write the response to it.
+                    response.ContentLength64 = buffer.Length;
+                    System.IO.Stream output = response.OutputStream;
+                    output.Write(buffer, 0, buffer.Length);
+                    // You must close the output stream.
+                    output.Close();
+                }
+                listener.Stop();
+            }
+        }
+
         public static void StartListeningOnAndRespondWith(string url, string? path, string? responseData)
         {
             if (!HttpListener.IsSupported)
