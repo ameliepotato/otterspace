b96fefdf68d721c0b584b84f917209bc27addc55
ameliepotato <11328479+ameliepotato@users.noreply.github.com>
2023-12-26 22:55:25 +0100

    chart js integration

---
+++ APV.Console/wwwroot/js/chartHistory.js
@@ -0,0 +1,20 @@
+﻿
+function loadChart(sensorId) {
+    var ctx = document.getElementById('chartImg' + sensorId);
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Label 1', 'Label 2', 'Label 3'],
+            datasets: [{
+                label: 'My Dataset',
+                data: [10, 20, 30],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            // additional configuration options
+        }
+    });
+}
